89e722c8713f16196a2afca23b2f16b52da2e028
Judy Trinh <jtrinh74@gmail.com>
2014-09-09 02:48:05 -0400

    Simgple camera follow, talisman prefab, lighting, materials

---
+++ Assets/GameplayCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayCamera : MonoBehaviour {
+
+    public Transform target;
+
+	void Start() {
+	
+	}
+
+	void Update() {
+        float x = target.transform.position.x;
+        float y = target.transform.position.y;
+
+        this.transform.position = new Vector3(x, y, this.transform.position.z);
+	}
+}
