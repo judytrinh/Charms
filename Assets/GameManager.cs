0e02a5055cf6b7ba009dab9b19e4c7c9228e9391
Judy Trinh <jtrinh74@gmail.com>
2014-09-09 09:49:16 -0400

    Added obstacles

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
     Object talismanPrefab;
+    Object obstaclePrefab;
     float TALISMAN_START_X;
     float TALISMAN_END_X;
     float TALISMAN_TOP_Y;
@@ -11,12 +12,15 @@ public class GameManager : MonoBehaviour {
 
 	void Start() {
         talismanPrefab = Resources.Load("Talisman");
+        obstaclePrefab = Resources.Load("Obstacle");
+
         TALISMAN_START_X = -5.0f;
         TALISMAN_END_X = 175.0f;
         TALISMAN_TOP_Y = 5.0f;
         TALISMAN_BOTTOM_Y = -5.0f;
         TALISMAN_NUM = 150;
 
+        // Instantiate talisman locations
         float interval = (TALISMAN_END_X - TALISMAN_START_X) / TALISMAN_NUM;
         for (int i = 0; i < TALISMAN_NUM; i++) {
             Vector3 pos = new Vector3(TALISMAN_START_X + interval * i, TALISMAN_TOP_Y, 0);
@@ -25,6 +29,14 @@ public class GameManager : MonoBehaviour {
         for (int i = 0; i < TALISMAN_NUM; i++) {
             Vector3 pos = new Vector3(TALISMAN_START_X + interval * i, TALISMAN_BOTTOM_Y, 0);
             GameObject talisman = Instantiate(talismanPrefab, pos, Quaternion.identity) as GameObject;
+        }
+
+        // Instantiate obstacle locations
+        for (int i = 0; i < TALISMAN_NUM / 4; i++) {
+            Vector3 pos = new Vector3(TALISMAN_START_X + interval * i * 4, Random.Range(TALISMAN_TOP_Y - 1.5f, TALISMAN_BOTTOM_Y + 1.5f), 0);
+            GameObject obstacle = Instantiate(obstaclePrefab, pos, Quaternion.identity) as GameObject;
+            float newScale = Random.Range(0.5f, 2.0f);
+            obstacle.transform.localScale = new Vector3(newScale, newScale, newScale);
         }
 	}
 
