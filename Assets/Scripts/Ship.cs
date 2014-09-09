d9f04a00d29313272b49c6677ec375a912bbd8af
Judy Trinh <jtrinh74@gmail.com>
2014-09-08 22:36:10 -0400

    Added ship movement with key inputs

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+    Vector3 SPEED;
+
+    void Start() {
+        SPEED = new Vector3(0.1f, 0.1f, 0);
+    }
+
+    void Update() {
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(new Vector3(0, SPEED.y, 0));
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.Translate(new Vector3(0, -SPEED.y, 0));
+        }
+
+        transform.Translate(new Vector3(SPEED.x, 0, 0));
+    }
+}
