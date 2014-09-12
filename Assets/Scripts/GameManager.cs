e8102413ca01dbf0df7bcf54170d4b3b5a89407f
Judy Trinh <jtrinh74@gmail.com>
2014-09-12 12:31:57 -0400

    Added flipping mechanic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,20 +20,25 @@ public class GameManager : MonoBehaviour {
         TALISMAN_BOTTOM_Y = -5.0f;
         TALISMAN_NUM = 150;
 
+        //====================================================================================================
         // Instantiate talisman locations
+        //====================================================================================================
         float interval = (TALISMAN_END_X - TALISMAN_START_X) / TALISMAN_NUM;
         for (int i = 0; i < TALISMAN_NUM; i++) {
-            Vector3 pos = new Vector3(TALISMAN_START_X + interval * i, TALISMAN_TOP_Y, 0);
+            Vector3 pos = new Vector3(TALISMAN_START_X + interval * i, TALISMAN_TOP_Y, 1);
             GameObject talisman = Instantiate(talismanPrefab, pos, Quaternion.identity) as GameObject;
+            talisman.GetComponent<Talisman>().Create(true, TALISMAN_TOP_Y, TALISMAN_BOTTOM_Y);
         }
         for (int i = 0; i < TALISMAN_NUM; i++) {
             Vector3 pos = new Vector3(TALISMAN_START_X + interval * i, TALISMAN_BOTTOM_Y, 0);
             GameObject talisman = Instantiate(talismanPrefab, pos, Quaternion.identity) as GameObject;
+            talisman.GetComponent<Talisman>().Create(false, TALISMAN_TOP_Y, TALISMAN_BOTTOM_Y);
         }
-
+        //====================================================================================================
         // Instantiate obstacle locations
+        //====================================================================================================
         for (int i = 0; i < TALISMAN_NUM / 4; i++) {
-            Vector3 pos = new Vector3(TALISMAN_START_X + interval * i * 4, Random.Range(TALISMAN_TOP_Y - 1.5f, TALISMAN_BOTTOM_Y + 1.5f), 0);
+            Vector3 pos = new Vector3(TALISMAN_START_X + interval * i * 4, Random.Range(TALISMAN_TOP_Y - 1.5f, TALISMAN_BOTTOM_Y + 1.5f), -1);
             GameObject obstacle = Instantiate(obstaclePrefab, pos, Quaternion.identity) as GameObject;
             float newScale = Random.Range(0.5f, 2.0f);
             obstacle.transform.localScale = new Vector3(newScale, newScale, newScale);
--- Assets/Scripts/Talisman.cs
+++ Assets/Scripts/Talisman.cs
@@ -2,20 +2,64 @@
 using System.Collections;
 
 public class Talisman : MonoBehaviour {
-    bool top;
+    bool atTheTop;
+    bool isChangingSides;
+
+    float TALISMAN_TOP_Y;
+    float TALISMAN_BOTTOM_Y;
+    float SPEED;
 
 	void Start() {
-        top = false;
+        atTheTop = false;
 	}
 
+    //====================================================================================================
     // Used as an initializer later when we want different types of talisman
     // TODO: write enum, include type as param
-    public void Create(Vector3 pos, bool top) {
-        transform.position = pos;
-        this.top = top;
+    //====================================================================================================
+    public void Create(bool top, float topY, float bottomY) {
+        this.atTheTop = top;
+        this.TALISMAN_TOP_Y = topY;
+        this.TALISMAN_BOTTOM_Y = bottomY;
+        this.SPEED = 0.2f;
+    }
+
+    //====================================================================================================
+    // Checks if we want to flip sides, if so inc/dec towards a side
+    //====================================================================================================
+    void FlipOrNot() {
+        if (isChangingSides) {
+            if (atTheTop) {
+                float x = this.transform.position.x;
+                float y = this.transform.position.y - SPEED;
+                float z = this.transform.position.z;
+                this.transform.position = new Vector3(x, y, z);
+
+                if (this.transform.position.y <= TALISMAN_BOTTOM_Y) {
+                    this.transform.position = new Vector3(this.transform.position.x, TALISMAN_BOTTOM_Y, this.transform.position.z);
+                    isChangingSides = false;
+                    atTheTop = false;
+                }
+            } else {
+                float x = this.transform.position.x;
+                float y = this.transform.position.y + SPEED;
+                float z = this.transform.position.z;
+                this.transform.position = new Vector3(x, y, z);
+
+                if (this.transform.position.y >= TALISMAN_TOP_Y) {
+                    this.transform.position = new Vector3(this.transform.position.x, TALISMAN_TOP_Y, this.transform.position.z);;
+                    isChangingSides = false;
+                    atTheTop = true;
+                }
+            }
+        }
     }
 	
 	void Update() {
-	    
+        if (Input.GetKey(KeyCode.Space)) {
+            isChangingSides = true;
+        }
+
+        FlipOrNot();
 	}
 }
