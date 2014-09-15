7f7fb8592b585e6ce7988da47a0f24c2489a7245
Judy Trinh <jtrinh74@gmail.com>
2014-09-15 10:37:52 -0400

    Added model

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,7 +7,7 @@ public class Ship : MonoBehaviour {
     Vector3 SPEED;
 
     void Start() {
-        SPEED = new Vector3(0.1f, 0.1f, 0);
+        SPEED = new Vector3(0.18f, 0.15f, 0);
 
         rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
     }
@@ -25,11 +25,11 @@ public class Ship : MonoBehaviour {
     void Update() {
 
         if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.Translate(new Vector3(0, SPEED.y, 0));
+            transform.Translate(new Vector3(0, SPEED.y, 0), Space.World);
         } else if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.Translate(new Vector3(0, -SPEED.y, 0));
+            transform.Translate(new Vector3(0, -SPEED.y, 0), Space.World);
         }
 
-        transform.Translate(new Vector3(SPEED.x, 0, 0));
+        transform.Translate(new Vector3(SPEED.x, 0, 0), Space.World);
     }
 }
